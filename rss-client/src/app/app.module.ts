71d87319d1db6bb548f7ba7584d444810705a063
Mark Hardebeck <mhardebeck@gmail.com>
2020-06-17 14:41:27 -0700

    added pages

--- rss-client/src/app/app.module.ts
+++ rss-client/src/app/app.module.ts
@@ -8,12 +8,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AccountPageComponent } from './Account/components/account-page/account-page.component';
 import { QuizPageComponent } from './Quiz/components/quiz-page/quiz-page.component';
-import { QuizQuesitonComponent } from './Quiz/components/quiz-quesiton/quiz-quesiton.component';
 import { ProfilePageComponent } from './Account/components/profile-page/profile-page.component';
 import { HeaderComponent } from './Common/header/header.component';
 import { EarnpointsOverviewPageComponent } from './Earnpoints/components/earnpoints-overview-page/earnpoints-overview-page.component';
 import { AccountSettingsPageComponent } from './Account/components/account-settings-page/account-settings-page.component';
-import { QuizInterimPageComponent } from './Quiz/components/quiz-interim-page/quiz-interim-page.component';
 import { LandingPageComponent } from './LandingPage/components/landing-page/landing-page.component';
 import { IndividualQuizPageComponent } from './Quiz/components/individual-quiz-page/individual-quiz-page.component';
 
@@ -22,23 +20,21 @@ import { IndividualQuizPageComponent } from './Quiz/components/individual-quiz-p
     AppComponent,
     AccountPageComponent,
     QuizPageComponent,
-    QuizQuesitonComponent,
     ProfilePageComponent,
     HeaderComponent,
     EarnpointsOverviewPageComponent,
     AccountSettingsPageComponent,
-    QuizInterimPageComponent,
     LandingPageComponent,
-    IndividualQuizPageComponent
+    IndividualQuizPageComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     ReactiveFormsModule,
-    NgbModule
+    NgbModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
