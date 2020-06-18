63be25f12b0b34df29426ba207e0521e498b5def
Mark Hardebeck <mhardebeck@gmail.com>
2020-06-17 19:43:57 -0700

    quiz page with search bar, and a earn point page kinda

--- rss-client/src/app/Quiz/components/quiz-page/quiz-page.component.ts
+++ rss-client/src/app/Quiz/components/quiz-page/quiz-page.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { QuizPageService } from 'src/app/Test/Quiz/quiz-page.service';
 
 @Component({
-  selector: 'app-quiz-page',
+  selector: 'quiz-page',
   templateUrl: './quiz-page.component.html',
-  styleUrls: ['./quiz-page.component.css']
+  styleUrls: ['./quiz-page.component.css'],
 })
 export class QuizPageComponent implements OnInit {
-
-  constructor() { }
+  constructor(private testservice: QuizPageService) {
+    this.quizData = this.testservice.getQuizData();
+  }
+  searchText: string;
+  quizData;
 
   ngOnInit(): void {
+    console.log(this.quizData);
   }
-
 }
