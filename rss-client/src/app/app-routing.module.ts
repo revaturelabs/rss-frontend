415170ab0860fe63d5c033cb97e272f6934f65b7
jpark1914 <parkerj4321@gmail.com>
2020-06-18 14:49:38 -0500

    Made the account-page

--- rss-client/src/app/app-routing.module.ts
+++ rss-client/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HeaderComponent } from './Common/header/header.component';
+import { AccountPageComponent } from './Account/components/account-page/account-page.component';
 
-const routes: Routes = [{ path: '', component: HeaderComponent }];
+const routes: Routes = [
+  { path: '', component: HeaderComponent },
+  {path: 'account/dashboard', component: AccountPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
