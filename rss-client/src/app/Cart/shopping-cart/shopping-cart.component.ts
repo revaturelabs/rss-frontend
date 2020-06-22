e7936f7103110998ec75b148dfeb80909960c299
Ben Swerdlow <benswerdlow1005@gmail.com>
2020-06-22 17:41:01 -0400

    Made infrastructure for cart and cart items: first draft

--- rss-client/src/app/Cart/shopping-cart/shopping-cart.component.ts
+++ rss-client/src/app/Cart/shopping-cart/shopping-cart.component.ts
@@ -1,18 +1,37 @@
 import { HostListener, Component, OnInit } from '@angular/core';
 // temporary fake products
 import { TempProducts } from '../temp_products';
+import { CartItem } from 'src/app/interfaces/cart-item.model';
+import { CartItemService } from 'src/app/services/cart-item.service';
+import { Cart } from 'src/app/interfaces/cart.model';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
-  styleUrls: ['./shopping-cart.component.css']
+  styleUrls: ['./shopping-cart.component.css'],
 })
 export class ShoppingCartComponent implements OnInit {
-TempProducts = TempProducts;
-mobile:boolean = false;
-  
+  TempProducts = TempProducts;
+  mobile: boolean = false;
 
-  constructor() { }
+  cart: Cart;
+  cartItemArray: CartItem[];
+
+  constructor(
+    private cartService: CartService,
+    private ciService: CartItemService
+  ) {
+    this.cartService
+      .getCartByUser_post(JSON.parse(window.sessionStorage.getItem('user')))
+      .subscribe(
+        (carts) =>
+          (this.cart = carts[parseInt(window.sessionStorage.getItem('index'))])
+      );
+    this.ciService
+      .getCartItemByCart_post(this.cart)
+      .subscribe((items) => (this.cartItemArray = items));
+  }
 
   ngOnInit(): void {
     // console.log(TempProducts);
@@ -24,21 +43,19 @@ mobile:boolean = false;
     } else {
       this.mobile = false;
       // console.log("not mobile");
-    }   
-
+    }
   }
 
   // listen for screen sizes
   @HostListener('window:resize', ['$event'])
   onResize(event) {
-      // responsive conditional
-      if (window.innerWidth < 550) {
-        this.mobile = true;
-        // console.log("mobile");
-      } else {
-        this.mobile = false;
-        // console.log("not mobile");
-      }   
+    // responsive conditional
+    if (window.innerWidth < 550) {
+      this.mobile = true;
+      // console.log("mobile");
+    } else {
+      this.mobile = false;
+      // console.log("not mobile");
+    }
   }
-
 }
