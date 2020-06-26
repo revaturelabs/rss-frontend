eba7ab08dcef32613437f94a375a7912ecb91d54
jpark1914 <parkerj4321@gmail.com>
2020-06-26 11:03:30 -0500

    In the midst of account services

--- rss-client/src/app/services/account.service.ts
+++ rss-client/src/app/services/account.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { User } from '../interfaces/user';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AccountService {
-  constructor(private httpclient: HttpClient) {}
+  constructor(private httpclient: HttpClient) { }
 
   updatePoints(acc: Account): Observable<Account> {
     return this.httpclient.post<any>(
@@ -15,24 +16,18 @@ export class AccountService {
     );
   }
 
-  addAccount(acc: Account): Observable<Account> {
-    return this.httpclient.post<any>(
-      'http://localhost:9000/account/addaccount',
-      acc
-    );
-  }
 
   getAccountByAccId(acc: Account): Observable<Account> {
     return this.httpclient.post<any>(
-      'http://localhost:9000/accuont/getaccountbyaccid',
+      'http://localhost:9000/account/account/ai',
       acc
     );
   }
 
-  getAccountByUserId(acc: Account): Observable<Account> {
+  getAccountByUserId(user: User): Observable<Account> {
     return this.httpclient.post<any>(
-      'http://localhost:9000/account/getaccountbyuserid',
-      acc
+      'http://localhost:9000/account/account/ui',
+      user
     );
   }
 }
