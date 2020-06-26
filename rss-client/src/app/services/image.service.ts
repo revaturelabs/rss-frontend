5180dab2c96998fa44086fd8e9549e9b238383d1
Brad Bingham <bnbingham@hotmail.com>
2020-06-26 13:13:59 -0700

    Added profilePic functionality

--- rss-client/src/app/services/image.service.ts
+++ rss-client/src/app/services/image.service.ts
@@ -1,15 +1,26 @@
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ImageService {
-  constructor() {}
+  url = 'http://localhost:9000';
+  constructor(private httpClient: HttpClient) {}
   setImage(input) {
     if (input == null) {
       return 'https://storage.googleapis.com/indie-hackers.appspot.com/product-avatars/quick-and-simple-image-placeholders/bcohuFwnmPgIu4aM56YZudq12m02';
     } else {
       return input;
     }
+  }
+  uploadImage(userId, picData) {
+    let update = {
+      userId,
+      profilePic: picData,
+    };
+    console.log(update);
+    return this.httpClient.post(this.url + '/user/pic', update);
   }
 }
