300dbef711c45203cd709054dadf1bc1f4ff5b4c
Siegfried <65792191+csieg150@users.noreply.github.com>
2020-09-02 09:54:51 -0500

    add some more listeners

--- rss-client/src/app/services/cheater.service.ts
+++ rss-client/src/app/services/cheater.service.ts
@@ -1,5 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
+
+/** CheaterService:
+ *  this service is here to track the user's mouse movements,
+ *  as well as certain keypresses and if they switch tabs.
+ *  This is designed to be combined with our 'Quiz' Component,
+ *  so that if the user tries to leave the page they'll get a
+ *  warning. If they continue, their quiz will be void.
+ */
 @Injectable({
   providedIn: 'root'
 })
@@ -7,44 +16,65 @@ export class CheaterService {
 
   hidden: string;
   visibilityChange: string;
+  confirmMessage: string;
+  leftTab: Subject<boolean>; // A special type of observable that we can use to easily communicate with our Quiz Component.
 
   constructor() {
-    if (typeof document.hidden !== "undefined") { // Opera 12.10 and Firefox 18 and later support 
-      this.hidden = "hidden";
-      this.visibilityChange = "visibilitychange";
-      
-      // Warn if the browser doesn't support addEventListener or the Page Visibility API
-      if (typeof document.addEventListener === "undefined" || this.hidden === undefined) {
-        console.log("This demo requires a browser, such as Google Chrome or Firefox, that supports the Page Visibility API.");
-      } else {
-      // Handle page visibility change
-        // console.log("Add visibility change listener");
-        // document.addEventListener(this.visibilityChange, this.handleVisibilityChange, false);
-        this.addListeners()
-      }
+    // Check if browser supports the 'hidden' attribute
+    if (typeof document.hidden !== 'undefined') { // Opera 12.10 and Firefox 18 and later support
+      this.hidden = 'hidden';
+      this.visibilityChange = 'visibilitychange';
     }
+    this.leftTab.next(false);
+    this.confirmMessage = 'Changing your tab or going to another window will void your quiz. \nDo you understand? \n\nClicking "no" will void the quiz.';
+
+      // Warn if the browser doesn't support addEventListener or the Page Visibility API 
+    if (typeof document.addEventListener === 'undefined' || this.hidden === undefined) {
+        console.log('This demo requires a browser, such as Google Chrome or Firefox, that supports the Page Visibility API.');
+      } else {
+        // Add event that tracks if the mouse leaves the HTML document entirely.
+        // It won't trigger if it enters a child node, only if they leave the boundries
+        // of their window.
+        this.addEvent(document, 'mouseout', (e) => {
+          e = e ? e : window.event;
+          const from = e.relatedTarget || e.toElement;
+          if (!from || from.nodeName == 'HTML') {
+              // the cursor has left the building
+              if (!confirm(this.confirmMessage)){
+                this.leftTab.next(true); // If they click 'no' on the confirm box, they void their quiz.
+              }
+          }
+        });
+        // Add an event listener for when the user deliberately changes tabs.
+        this.addEvent(document, this.visibilityChange, this.handleVisibilityChange);
+
+        this.addEvent(document,'keypress',this.logKey);
+      }
   }
 
-  // If the page is hidden, pause the video;
-  // if the page is shown, play the video
+  // If the page is hidden (after switching tabs typically), change the subject;
   handleVisibilityChange(): void {
     if (document.hidden) {
-      console.log("HIDING")
-      alert("Tab left, quiz void");
+      alert('Tab left, quiz void');
+      this.leftTab.next(true);
     }
   }
 
-  addListeners() {
-    addEventListener('focus', (event) => {
-      // event.preventDefault()
-      console.log('some shit changed')   
-    }, true);
-    
-    addEventListener('blur', (event) => {
-      event.preventDefault()
-      console.log('some shit changed')
-      // alert('tf you think youre doing')
-    }, true);
+  logKey(e): void{
+    if (e.code == 'AltLeft' || e.code == 'AltRight'){
+      alert('Stop pressing Alt!');
+    }
+  }
+
+  // Support for different kinds of browsers.
+ addEvent(obj, evt, fn) {
+  if (obj.addEventListener) {
+      obj.addEventListener(evt, fn, false);
+  }
+  else if (obj.attachEvent) {
+      obj.attachEvent('on' + evt, fn);
+  }
+
   }
 
 }
