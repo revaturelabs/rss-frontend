7b51b9b66e6fd2aa22ac7ecb22d1bf7fb0dd0ca1
bescoto98 <escoto.brenda42@gmail.com>
2020-10-01 11:55:34 -0700

    more changes to new quiz module

--- rss-client/src/app/Quiz1/quiz1.module.ts
+++ rss-client/src/app/Quiz1/quiz1.module.ts
@@ -1,12 +1,52 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
+import { AddQuizComponent } from './admin/add-quiz/add-quiz.component';
+import { EditQuizComponent } from './admin/edit-quiz/edit-quiz.component';
+import { QuizFormComponent } from './admin/quiz-form/quiz-form.component';
+
+import { CheaterWarningComponent } from './employee/cheater-warning/cheater-warning.component';
+import { IndividualQuizPageComponent } from './employee/individual-quiz-page/individual-quiz-page.component';
+import { InvalidQuizComponent, InvalidQuizDialogContent } from './employee/invalid-quiz/invalid-quiz.component';
+import { PostTestComponent } from './employee/post-test/post-test.component';
+import { PreTestComponent } from './employee/pre-test/pre-test.component';
+import { QuizPageComponent } from './employee/quiz-page/quiz-page.component';
+import { TestInProgressComponent } from './employee/test-in-progress/test-in-progress.component';
+
+import { Questions } from './models/questions';
+import { Quiz } from './models/quiz';
+import { QuizSubmit } from './models/quizSubmit';
+import { Subject } from './models/subject';
+import { TestQuizPageData } from './models/test-quiz-page-data';
+
+import { CheaterService, DialogContent } from './service/cheater.service';
+import { QuizPageService } from './service/quiz-page.service';
+import { QuizService } from './service/quiz-service';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AddQuizComponent,
+    EditQuizComponent,
+    QuizFormComponent,
+    CheaterWarningComponent,
+    IndividualQuizPageComponent,
+    InvalidQuizComponent,
+    PostTestComponent,
+    PreTestComponent,
+    QuizPageComponent,
+    TestInProgressComponent,
+    DialogContent,
+    InvalidQuizDialogContent
+  ],
   imports: [
     CommonModule
+  ],
+  providers:[
+    CheaterService,
+    QuizPageService,
+    QuizService
   ]
 })
 export class Quiz1Module { }
--- rss-client/src/app/app.module.ts
+++ rss-client/src/app/app.module.ts
@@ -6,56 +6,57 @@ import { NgbModule, NgbActiveModal, NgbModalModule } from '@ng-bootstrap/ng-boot
 import { FilterPipe } from './filter.pipe';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { QuizPageComponent } from './Quiz/components/quiz-page/quiz-page.component';
+// import { QuizPageComponent } from './Quiz/components/quiz-page/quiz-page.component';
 import { HeaderComponent } from './Common/header/header.component';
 import { EarnpointsOverviewPageComponent } from './Earnpoints/components/earnpoints-overview-page/earnpoints-overview-page.component';
 import { LandingPageComponent } from './LandingPage/components/landing-page/landing-page.component';
-import { IndividualQuizPageComponent } from './Quiz/components/individual-quiz-page/individual-quiz-page.component';
-import { PreTestComponent } from './Quiz/components/pre-test/pre-test.component';
-import { TestInProgressComponent } from './Quiz/components/test-in-progress/test-in-progress.component';
-import { PostTestComponent } from './Quiz/components/post-test/post-test.component';
+// import { IndividualQuizPageComponent } from './Quiz/components/individual-quiz-page/individual-quiz-page.component';
+// import { PreTestComponent } from './Quiz/components/pre-test/pre-test.component';
+// import { TestInProgressComponent } from './Quiz/components/test-in-progress/test-in-progress.component';
+// import { PostTestComponent } from './Quiz/components/post-test/post-test.component';
 import { ShoppingCartComponent } from './Cart/shopping-cart/shopping-cart.component';
 import { StoreModule } from '@ngrx/store';
 import { reducer } from './store/reducers/user.reducer';
 import { AdminPageComponent } from './Admin/admin-page/admin-page.component';
-import { AdminQuizComponent } from './Admin/admin-quiz/admin-quiz.component';
+// import { AdminQuizComponent } from './Admin/admin-quiz/admin-quiz.component';
 import { AdminInventoryComponent } from './Admin/admin-inventory/admin-inventory.component';
-import { AddQuizComponent } from './Quiz/admin/add-quiz/add-quiz.component';
-import { EditQuizComponent } from './Quiz/admin/edit-quiz/edit-quiz.component';
-import { QuizFormComponent } from './Quiz/admin/quiz-form/quiz-form.component';
+// import { AddQuizComponent } from './Quiz/admin/add-quiz/add-quiz.component';
+// import { EditQuizComponent } from './Quiz/admin/edit-quiz/edit-quiz.component';
+// import { QuizFormComponent } from './Quiz/admin/quiz-form/quiz-form.component';
 import { SelectCartComponent } from './Cart/select-cart/select-cart.component';
 import { UserModule } from './User/user/user.module';
 import { AppInventoryModule } from "../app/app-inventory/app-inventory.module";
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { CheaterWarningComponent, DialogContent } from './Quiz/components/cheater-warning/cheater-warning.component';
+// import { CheaterWarningComponent, DialogContent } from './Quiz/components/cheater-warning/cheater-warning.component';
 import { MatDialogModule, MatDialogActions } from '@angular/material/dialog';
-import {MatButtonModule} from '@angular/material/button';
-import { InvalidQuizComponent, InvalidQuizDialogContent } from './Quiz/components/invalid-quiz/invalid-quiz.component';
+import { MatButtonModule } from '@angular/material/button';
+// import { InvalidQuizComponent, InvalidQuizDialogContent } from './Quiz/components/invalid-quiz/invalid-quiz.component';
+import { Quiz1Module } from './Quiz1/quiz1.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuizPageComponent,
+    // QuizPageComponent,
     HeaderComponent,
     EarnpointsOverviewPageComponent,
     LandingPageComponent,
-    IndividualQuizPageComponent,
+    // IndividualQuizPageComponent,
     FilterPipe,
-    PreTestComponent,
-    TestInProgressComponent,
-    PostTestComponent,
+    // PreTestComponent,
+    // TestInProgressComponent,
+    // PostTestComponent,
     ShoppingCartComponent,
     AdminPageComponent,
-    AdminQuizComponent,
+    // AdminQuizComponent,
     AdminInventoryComponent,
-    AddQuizComponent,
-    EditQuizComponent,
-    QuizFormComponent,
+    // AddQuizComponent,
+    // EditQuizComponent,
+    // QuizFormComponent,
     SelectCartComponent,
-    CheaterWarningComponent,
-    DialogContent,
-    InvalidQuizComponent,
-    InvalidQuizDialogContent
+    // CheaterWarningComponent,
+    // DialogContent,
+    // InvalidQuizComponent,
+    // InvalidQuizDialogContent
   ],
   imports: [
     UserModule,
@@ -70,6 +71,7 @@ import { InvalidQuizComponent, InvalidQuizDialogContent } from './Quiz/component
     NoopAnimationsModule,
     MatDialogModule,
     MatButtonModule,
+    Quiz1Module
   ],
   providers: [
     SelectCartComponent,
