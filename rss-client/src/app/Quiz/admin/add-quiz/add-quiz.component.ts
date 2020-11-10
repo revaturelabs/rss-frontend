ab34c40a9b3f9c05c3da2caa1d1f5d2268d4366a
Ehimare Agboneni <ehimare.agboneni@gmail.com>
2020-11-10 13:47:54 -0500

    edited the modals and added a button

--- rss-client/src/app/Quiz/admin/add-quiz/add-quiz.component.ts
+++ rss-client/src/app/Quiz/admin/add-quiz/add-quiz.component.ts
@@ -27,6 +27,7 @@ export class AddQuizComponent implements OnInit {
   foo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
   looper = [1]
   isATP = 0;
+  newOption:String;
 
   focusedQuiz = {
     quizId: 0,
