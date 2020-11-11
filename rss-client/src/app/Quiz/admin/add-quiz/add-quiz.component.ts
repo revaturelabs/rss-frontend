c71288fb5abd6f83a1cb5a808b9e0a682832f32a
Ehimare Agboneni <ehimare.agboneni@gmail.com>
2020-11-11 16:34:50 -0500

    implemented an add and remove button for options on the add-quiz and edit-quiz html

--- rss-client/src/app/Quiz/admin/add-quiz/add-quiz.component.ts
+++ rss-client/src/app/Quiz/admin/add-quiz/add-quiz.component.ts
@@ -56,6 +56,10 @@ export class AddQuizComponent implements OnInit {
     this.options.pop();
   }
 
+  removeOption(){
+    this.options.pop();
+  }
+
   validate() {
     if (this.focusedQuiz.quizTopic && this.focusedQuiz.questions.length > 0) {
       this.isValid = true;
