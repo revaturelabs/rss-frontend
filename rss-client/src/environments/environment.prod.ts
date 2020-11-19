2fa96a42fe0e54f5e488aa611652d106f48bdcd9
thecheatisgod@gmail.com <thecheatisgod@gmail.com>
2020-11-19 09:49:30 -0700

    fixed production URLs

--- rss-client/src/environments/environment.prod.ts
+++ rss-client/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  accountServiceUrl: 'http://a7337c3f816f34896895bf66fa583e39-538606133.us-east-1.elb.amazonaws.com/account',
-  evaluationServiceUrl: 'http://a7337c3f816f34896895bf66fa583e39-538606133.us-east-1.elb.amazonaws.com/evaluation',
-  cartServiceUrl: 'http://a7337c3f816f34896895bf66fa583e39-538606133.us-east-1.elb.amazonaws.com/cart',
-  inventoryServiceUrl: 'http://a7337c3f816f34896895bf66fa583e39-538606133.us-east-1.elb.amazonaws.com/inventory',
+  accountServiceUrl: 'http://a05ca03ed94f848dfa2d6ef975d395ff-284278107.us-east-1.elb.amazonaws.com/account',
+  evaluationServiceUrl: 'http://a05ca03ed94f848dfa2d6ef975d395ff-284278107.us-east-1.elb.amazonaws.com/evaluation',
+  cartServiceUrl: 'http://a05ca03ed94f848dfa2d6ef975d395ff-284278107.us-east-1.elb.amazonaws.com/cart',
+  inventoryServiceUrl: 'http://a05ca03ed94f848dfa2d6ef975d395ff-284278107.us-east-1.elb.amazonaws.com/inventory',
   admin: true,
 };
